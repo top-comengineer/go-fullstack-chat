14d571ca7cf1bf3f3ca3caa291a6dddba1b96941
top-comengineer <yunlong870924@gmail.com>
2023-03-24 10:31:29 -0700

    Update index.js
    Co-Authored-By: StephenTanevski <112393894+StephenTanevski@users.noreply.github.com>

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,25 +1,26 @@
-// api/index.js
-var socket = new WebSocket('ws://localhost:9000/ws');
+//file name:  api/index.js
+//description: total api integration.
+var socket = new WebSocket("ws://localhost:9000/ws");
 
 let connect = (cb) => {
-  console.log("connecting")
+  console.log("connecting");
 
   socket.onopen = () => {
     console.log("Successfully Connected");
-  }
-  
+  };
+
   socket.onmessage = (msg) => {
     console.log("Message from WebSocket: ", msg);
     cb(msg);
-  }
+  };
 
   socket.onclose = (event) => {
-    console.log("Socket Closed Connection: ", event)
-  }
+    console.log("Socket Closed Connection: ", event);
+  };
 
   socket.onerror = (error) => {
-    console.log("Socket Error: ", error)
-  }
+    console.log("Socket Error: ", error);
+  };
 };
 
 let sendMsg = (msg) => {
